cfd9b695b28de0eb221488b7263e2e243ef7b5fe
PHAM Huy <animephamhuy@gmail.com>
2025-05-23 10:44:47 +0200

    Ajouter la test division

--- Calculatrice_test.java
+++ Calculatrice_test.java
@@ -14,4 +14,9 @@ public class Calculatrice_test{
     public void Test_addition(){
         assertEquals(10,calc.add(8,2)); // Il va etre true
     }
+
+    @Test
+    public void Test_div(){
+        assertEquals(4,calc.div(100,25));//il va etre true
+    }
 }
