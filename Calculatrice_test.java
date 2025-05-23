9da61d8a765e0bbfc9c321819ce6b9697224eeae
PHAM Huy <animephamhuy@gmail.com>
2025-05-23 10:42:56 +0200

    Ajouter la test de l'addition

--- Calculatrice_test.java
+++ Calculatrice_test.java
@@ -1,3 +1,17 @@
-public class Calculatrice_test extends Calculatrice {
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
+public class Calculatrice_test{
+    public Calculatrice calc;
+    @BeforeEach
+    public void init(){
+        calc = new Calculatrice();
+    }
+
+    @Test
+    public void Test_addition(){
+        assertEquals(10,calc.add(8,2)); // Il va etre true
+    }
 }
